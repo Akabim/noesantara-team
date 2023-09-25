43ac7eb5cf3281284ebc4ce030c9f591630e6cf8
Akabim <abimanyu.ari.d.123@gmail.com>
2023-09-26 00:51:24 +0700

    v.1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "primary-font": ["Chakra Petch", "sans-serif"],
+        "secondary-font": ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
