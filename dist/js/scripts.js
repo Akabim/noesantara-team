0e7e783960b68b61a1f8367b2e736dc4efe983f4
Akabim <abimanyu.ari.d.123@gmail.com>
2023-10-02 09:58:59 +0700

    work2

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,12 +1,12 @@
 // Burger menus
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // open
     const burger = document.querySelectorAll('.navbar-burger');
     const menu = document.querySelectorAll('.navbar-menu');
 
     if (burger.length && menu.length) {
         for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
+            burger[i].addEventListener('click', function () {
                 for (var j = 0; j < menu.length; j++) {
                     menu[j].classList.toggle('hidden');
                 }
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (close.length) {
         for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
+            close[i].addEventListener('click', function () {
                 for (var j = 0; j < menu.length; j++) {
                     menu[j].classList.toggle('hidden');
                 }
@@ -30,11 +30,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (backdrop.length) {
         for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
+            backdrop[i].addEventListener('click', function () {
                 for (var j = 0; j < menu.length; j++) {
                     menu[j].classList.toggle('hidden');
                 }
             });
         }
     }
+});
+
+
+var sendButton = document.querySelector('#send-button');
+sendButton.addEventListener('click', function () {
+    Swal.fire(
+        'Pesan Terkirim!',
+        'Akan segera kami balas melalui email.',
+        'success'
+    )
 });
